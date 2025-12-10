24f44ba7a5dbf2c499fd55facce5c29e3d0f5c45
Thomas Delmotte <marcodlmt@gmail.com>
2025-12-10 11:56:54 +0100

    feat rechercher find

--- workspace/script.js
+++ workspace/script.js
@@ -2,25 +2,14 @@ let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
 
 	
 db = db.getSiblingDB('technocite');
-// SE connecter a la base de donnees technocite Si elle n'existe pas elle sera créee
-// IDEM que use dans le shell mongo
 
 
 // INSERT INTO students(name) VALUES ("Jean Sébastien")
-const inserted = db.formators.insertOne({
-    name:"Gilles Dupont",
+const students = db.students.find({
+    name:"Jean Sébastien"
 });
 
 
-const insertedMany = db.formators.insertMany({
-    name:"Gilles Dupont",
-},
-{
-    name:"Fred Gérard",
-},
-{
-    name:"Gilles François",
-}
-);
 
-console.log(inserted);
+
+console.log(students);
