530cf3befddc07a946b1cdf9381daeb88473c088
Thomas Delmotte <marcodlmt@gmail.com>
2025-12-10 11:48:59 +0100

    feat Creer une collection

--- workspace/script.js
+++ workspace/script.js
@@ -1,38 +1,26 @@
-let db = connect("mongodb://root:test123@localhost");
-// équivalent du "use technocite"
-db = db.getSiblingDB("sample_mflix");
+let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
 
-db.movies.aggregate([
-    {
-        $match: {
-            'imdb.rating': {
-                $lt: 5
-            }
-        }
-    },
-    {
-        $unwind: '$directors'
-    },
-    {
-        $group: {
-            _id: '$directors',
-            total: {
-                $count: {}
-            }
-        }
-    },
-    {
-        $sort: {
-            total: -1
-        }
-    },
-    {
-        $limit: 10
-    },
-    {
-        $out : {
-            db: 'sample_mflix',
-            coll: 'lame_directors'
-        }
-    }
-]);
+	
+db = db.getSiblingDB('technocite');
+// SE connecter a la base de donnees technocite Si elle n'existe pas elle sera créee
+// IDEM que use dans le shell mongo
+
+
+// INSERT INTO students(name) VALUES ("Jean Sébastien")
+const inserted = db.formators.insertOne({
+    name:"Gilles Dupont",
+});
+
+
+const insertedMany = db.formators.insertMany({
+    name:"Gilles Dupont",
+},
+{
+    name:"Fred Gérard",
+},
+{
+    name:"Gilles François",
+}
+);
+
+console.log(inserted);
