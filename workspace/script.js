1b458b4e866699782a898f3990e53e2efe5c1f92
Thomas Delmotte <marcodlmt@gmail.com>
2025-12-10 14:33:17 +0100

    feat Recherche BETWEEN - Mongo

--- workspace/script.js
+++ workspace/script.js
@@ -4,17 +4,16 @@ db = db.getSiblingDB('sample_mflix');
 
 
 
-//Exercice : 1
 
-//Dans lesquels a joué Keanu Reeves
-
-const moviesEx1 = db.movies
+// Exercice : 3
+// sortis entre 2002 et 2008
+const moviesEx3 = db.movies
 .find({
-    actors : {
-        $in: ["Keanu Reeves"]
+    released: {
+        $gte: ISODate("2002-01-01T00:00:00Z"),
+        $lte: ISODate("2008-12-31T23:59:59Z")
     }
 })
 
 
-console.log(moviesEx1);
-
+console.log(moviesEx3);
